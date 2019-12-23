7e36f92dc95e4668cbdbf66b0c64d79ae1d8ba5c
Tucker <tmiles7@vols.utk.edu>
2019-12-22 19:50:34 -0500

    TTT game works correctly

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -39,7 +39,13 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if(winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -78,6 +84,27 @@ function Square(props) {
         </div>
       );
     }
+  }
+
+  //Given an array of 9 squares, this function will check for a winner and return 'X', 'O', or null as appropriate.
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   // ========================================

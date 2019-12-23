91c1d5d3b4128739e75c7a23ab4da7591118635b
Tucker <tmiles7@vols.utk.edu>
2019-12-22 19:44:26 -0500

    "X" and "O" alternating correctly

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-
-    render() {
+//Function Component
+function Square(props) {
       return (
         <button className="square" 
-        onClick={() => this.props.onClick() }   /*not 100% on the arrow function syntax.*/
+        onClick={() => props.onClick() }   /*not 100% on the arrow function syntax.*/
         > 
-          {this.props.value}
+          {props.value}
         </button>
       );
-    }
   }
   
   class Board extends React.Component {
@@ -20,13 +18,17 @@ class Square extends React.Component {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
-      }
+        xIsNext: true,
+      };
   }
 
     handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
 
     renderSquare(i) {
